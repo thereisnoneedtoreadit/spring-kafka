43f4e8eeaed2f0fe5457fab7ddb031ab0beaff1d
ashashnin <ashashnin@tsconsulting.com>
2020-04-17 22:01:28 +0300

    init commit

---
+++ src/main/java/com/example/springkafkastudy/SpringKafkaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springkafkastudy/SpringKafkaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
