99398453341dce02b4660528eb5831f4cb3284f7
ashashnin <ashashnin@tsconsulting.com>
2020-04-19 20:45:09 +0300

    add kafka config

--- src/main/java/com/example/springkafkastudy/controller/TaskController.java
+++ src/main/java/com/example/springkafkastudy/controller/TaskController.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
