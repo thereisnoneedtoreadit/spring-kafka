912cbb3c80afc8f686f5111116c5c21f352926b7
ashashnin <ashashnin@tsconsulting.com>
2020-04-19 17:30:06 +0300

    add returning value to controller

--- src/main/java/com/example/springkafkastudy/controller/TaskController.java
+++ src/main/java/com/example/springkafkastudy/controller/TaskController.java
@@ -2,14 +2,17 @@ package com.example.springkafkastudy.controller;
 
 import com.example.springkafkastudy.model.dto.TaskDto;
 import com.example.springkafkastudy.service.TaskService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @RestController
-@RequestMapping(value = "/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/tasks", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
 public class TaskController {
 
     private final TaskService taskService;
@@ -20,10 +23,10 @@ public class TaskController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public Mono<Void> create(@RequestBody Mono<TaskDto> task) {
+    public Mono<TaskDto> create(@RequestBody Mono<TaskDto> task) {
+        log.debug("Received request for task creating");
         return task.publishOn(Schedulers.elastic())
-                .map(taskService::create)
-                .then();
+                .map(taskService::create).doOnNext(Mono::just);
     }
 
 }
