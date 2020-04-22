756aaa216da760b50d61bdb4268e4476e2b725b3
ashashnin <ashashnin@tsconsulting.com>
2020-04-22 15:52:36 +0300

    add save to db

--- src/main/java/com/example/springkafkastudy/model/dto/TaskDto.java
+++ src/main/java/com/example/springkafkastudy/model/dto/TaskDto.java
@@ -1,10 +1,12 @@
 package com.example.springkafkastudy.model.dto;
 
+import com.example.springkafkastudy.model.entity.Task;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +16,9 @@ public class TaskDto {
     private String name;
     private String description;
     private Instant time;
+
+    public Task asEntity() {
+        return new Task(UUID.randomUUID(), this.name, this.description, this.time);
+    }
 
 }
--- src/main/java/com/example/springkafkastudy/service/TaskService.java
+++ src/main/java/com/example/springkafkastudy/service/TaskService.java
@@ -2,8 +2,9 @@ package com.example.springkafkastudy.service;
 
 import com.example.springkafkastudy.exception.GlobalException;
 import com.example.springkafkastudy.model.dto.TaskDto;
+import com.example.springkafkastudy.model.entity.Task;
 import com.example.springkafkastudy.producer.TaskProducer;
-import com.example.springkafkastudy.serde.serializer.TaskSerializer;
+import com.example.springkafkastudy.repository.TaskRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,11 @@ import org.springframework.stereotype.Service;
 public class TaskService {
 
     private final TaskProducer taskProducer;
+    private final TaskRepository taskRepository;
 
-    public TaskService(TaskProducer taskProducer) {
+    public TaskService(TaskProducer taskProducer, TaskRepository taskRepository) {
         this.taskProducer = taskProducer;
+        this.taskRepository = taskRepository;
     }
 
     public void sendTaskToKafka(TaskDto taskDto) {
@@ -28,7 +31,9 @@ public class TaskService {
     }
 
     public void saveTask(TaskDto taskDto) {
-        System.out.println(taskDto);
+        Task task = taskDto.asEntity();
+        taskRepository.save(task);
+        log.info("Saved: {}", task);
     }
 
 }
